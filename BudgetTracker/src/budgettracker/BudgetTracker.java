aa23416b37b8b6902152dd83583c14947286ae04
henryd1213 <78320788+henryd1213@users.noreply.github.com>
2021-06-04 16:49:38 -0400

    Update BudgetTracker.java

--- BudgetTracker/src/budgettracker/BudgetTracker.java
+++ BudgetTracker/src/budgettracker/BudgetTracker.java
@@ -25,7 +25,7 @@ import javafx.scene.control.Alert.AlertType;
  * @author henrydare
  */
 public class BudgetTracker extends Application {
-    
+
     public DatePicker date;
     public TextField incomeAmount;
     public ComboBox savingTarget;
@@ -44,10 +44,9 @@ public class BudgetTracker extends Application {
     public Text overperforming;
     public Text meeting;
 
-    
     @Override
     public void start(Stage primaryStage) {
-        
+
         Rectangle backgroundBox = new Rectangle(25, 25, 250, 115);
         backgroundBox.setStroke(Color.BLACK);
         backgroundBox.setFill(null);
@@ -63,11 +62,11 @@ public class BudgetTracker extends Application {
         savingTarget = new ComboBox();
         savingTarget.getItems().addAll("0%", "5%", "10%", "15%", "20%", "25%", "30%", "35%", "40%", "45%", "50%",
                 "55%", "60%", "65%", "70%", "75%", "80%", "85%", "90%", "95%", "100%");
+        savingTarget.setValue("0%");
         savingTarget.setLayoutX(165);
         savingTarget.setLayoutY(94);
         savingTarget.setPrefWidth(80);
-        
-        
+
         Rectangle trackingBox = new Rectangle(25, 165, 250, 300);
         trackingBox.setStroke(Color.BLACK);
         trackingBox.setFill(null);
@@ -80,7 +79,7 @@ public class BudgetTracker extends Application {
         date.setLayoutX(85);
         date.setLayoutY(202);
         Text amountLabel = new Text(50, 252, "Amount in Dollars:");
-        amount = new TextField();
+        amount = new TextField("0");
         amount.setPrefWidth(65);
         amount.setPrefHeight(5);
         amount.setLayoutX(155);
@@ -88,21 +87,24 @@ public class BudgetTracker extends Application {
         Text directionLabel = new Text(50, 282, "Direction:");
         flowDirection = new ComboBox();
         flowDirection.getItems().addAll("Deposit", "Withdrawl");
+        flowDirection.setValue("Choose");
         flowDirection.setLayoutX(110);
         flowDirection.setLayoutY(266);
         flowDirection.setPrefWidth(110);
         Text expenseTypeLabel = new Text(50, 314, "Expense Type:");
         expenseCategory = new ComboBox();
-        expenseCategory.getItems().addAll("Housing Mortgage","Housing Rent", "Home Insurance", "Property Taxes", "Utilities","Food - Groceries","Food - Resturaunts",
-                "Transportation - Car Payment","Transportation - Fuel", "Transportation - Repairs", "Transportation - Insurance", "Transportation - licenses / Fees",
-                "Clothing", "Medical - Doctor Bills","Medical - Medications", "Medical - Insurance", "Medical - Miscellaneous", "Personal", "Other");
+        expenseCategory.getItems().addAll("Housing Mortgage", "Housing Rent", "Home Insurance", "Property Taxes", "Utilities", "Food - Groceries", "Food - Resturaunts",
+                "Transportation - Car Payment", "Transportation - Fuel", "Transportation - Repairs", "Transportation - Insurance", "Transportation - licenses / Fees",
+                "Clothing", "Medical - Doctor Bills", "Medical - Medications", "Medical - Insurance", "Medical - Miscellaneous", "Personal", "Other");
+        expenseCategory.setValue("Choose");
         expenseCategory.setLayoutX(50);
         expenseCategory.setLayoutY(324);
         expenseCategory.setPrefWidth(220);
         expenseCategory.setPrefHeight(5);
         Text savingTypeLabel = new Text(50, 372, "Deposit Type:");
         savingCategory = new ComboBox();
-        savingCategory.getItems().addAll("Savings - General","Emergency Fund", "College Fund", "Retirement Fund");
+        savingCategory.getItems().addAll("Savings - General", "Emergency Fund", "College Fund", "Retirement Fund");
+        savingCategory.setValue("Choose");
         savingCategory.setLayoutX(50);
         savingCategory.setLayoutY(382);
         savingCategory.setPrefWidth(220);
@@ -126,18 +128,18 @@ public class BudgetTracker extends Application {
         delete.setLayoutX(215);
         delete.setLayoutY(420);
         delete.setOnAction(this::delete);
-        
+
         Rectangle goalBox = new Rectangle(300, 25, 1100, 232);
         goalBox.setStroke(Color.BLACK);
         goalBox.setFill(null);
-        Text goalLabel = new Text(800, 50, "Top Budget Goals"); 
+        Text goalLabel = new Text(800, 50, "Top Budget Goals");
         goalLabel.setFill(Color.BLUE);
         Text goal1Label = new Text(325, 85, "Goal 1:");
         goal1 = new TextArea();
         goal1.setPrefWidth(1000);
         goal1.setPrefHeight(5);
         goal1.setLayoutX(375);
-        goal1.setLayoutY(62);             
+        goal1.setLayoutY(62);
         Text goal2Label = new Text(325, 150, "Goal 2:");
         goal2 = new TextArea();
         goal2.setPrefWidth(1000);
@@ -149,80 +151,80 @@ public class BudgetTracker extends Application {
         goal3.setPrefWidth(1000);
         goal3.setPrefHeight(5);
         goal3.setLayoutX(375);
-        goal3.setLayoutY(192);        
-                
-        
+        goal3.setLayoutY(192);
+
         Rectangle budgetStandingBox = new Rectangle(300, 282, 1100, 183);
         budgetStandingBox.setStroke(Color.BLACK);
-        budgetStandingBox.setFill(null); 
-        Text budgetStandingLabel = new Text(800, 307,"Budget Goal Standing");
+        budgetStandingBox.setFill(null);
+        Text budgetStandingLabel = new Text(800, 307, "Budget Goal Standing");
         budgetStandingLabel.setFill(Color.BLUE);
-        underperforming = new Text(475, 380,"Underperforming");
+        underperforming = new Text(475, 380, "Underperforming");
         underperforming.setStyle("-fx-font-size: 24");
         underperforming.setFill(null);
         underperforming.setStroke(Color.BLACK);
-        meeting = new Text(800, 380,"Meeting");
+        meeting = new Text(800, 380, "Meeting");
         meeting.setStyle("-fx-font-size: 24");
         meeting.setFill(null);
         meeting.setStroke(Color.BLACK);
-        overperforming = new Text(1025, 380,"Overperforming");
+        overperforming = new Text(1025, 380, "Overperforming");
         overperforming.setStyle("-fx-font-size: 24");
         overperforming.setFill(null);
         overperforming.setStroke(Color.BLACK);
 
         Rectangle budgetChartBox = new Rectangle(25, 490, 1377, 380);
         budgetChartBox.setStroke(Color.BLACK);
-        budgetChartBox.setFill(null); 
+        budgetChartBox.setFill(null);
         Text budgetChartBoxLabel = new Text(690, 515, "Budget Chart");
         budgetChartBoxLabel.setFill(Color.BLUE);
 
+        Group backgroundItems = new Group(backgroundBox, backgroundLabel, incomeLabel, incomeAmount, saveTargetLabel, savingTarget);
 
-        
-        Group backgroundItems = new Group(backgroundBox,backgroundLabel, incomeLabel, incomeAmount, saveTargetLabel, savingTarget);
-        
         Group trackingItems = new Group(trackingBox, trackingLabel, dateLabel, date, amountLabel, amount, directionLabel, flowDirection, expenseTypeLabel, expenseCategory, savingTypeLabel, savingCategory, update, save, load, delete);
-        
+
         Group budgetGoalItems = new Group(goalBox, goalLabel, goal1Label, goal1, goal2Label, goal2, goal3Label, goal3);
-        
+
         Group budgetStandingItems = new Group(budgetStandingBox, budgetStandingLabel, underperforming, meeting, overperforming);
-        
+
         Group budgetChartItems = new Group(budgetChartBox, budgetChartBoxLabel);
-      
+
         Group root = new Group(backgroundItems, trackingItems, budgetGoalItems, budgetStandingItems, budgetChartItems);
-        
+
         Scene scene = new Scene(root, 1000, 1000);
-        
+
         primaryStage.setTitle("Budget Tracker");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-    
-        public void update(ActionEvent event){
-        
-            String incomeTest=incomeAmount.getText();
-boolean incorrect = true;
-while(incorrect){
-try{Double.parseDouble(incomeTest);incorrect = false;}
-catch(NumberFormatException nfe){
-   incomeTest="0";
-   Alert a = new Alert(AlertType.ERROR);
-   a.setContentText("Invalid data type");
-   a.show();
- }
-}
 
-String amountTest=amount.getText();
-boolean incorrect2 = true;
-while(incorrect2){
-try{Double.parseDouble(amountTest);incorrect2 = false;}
-catch(NumberFormatException nfe){
-   amountTest="0";
-   Alert a = new Alert(AlertType.ERROR);
-   a.setContentText("Invalid data type");
-   a.show();
- }
-}
-            
+    public void update(ActionEvent event) {
+        String incomeTest = incomeAmount.getText();
+        boolean incorrect = true;
+        while (incorrect) {
+            try {
+                Double.parseDouble(incomeTest);
+                incorrect = false;
+            } catch (NumberFormatException nfe) {
+                incomeTest = "0";
+                Alert a = new Alert(AlertType.ERROR);
+                a.setContentText("Invalid data type");
+                a.show();
+            }
+        }
+
+        String amountTest = amount.getText();
+        boolean incorrect2 = true;
+        while (incorrect2) {
+            try {
+                Double.parseDouble(amountTest);
+                incorrect2 = false;
+            } catch (NumberFormatException nfe) {
+                amountTest = "0";
+                Alert a = new Alert(AlertType.ERROR);
+                a.setContentText("Invalid data type");
+                a.show();
+            }
+        }
+
         int biweekIncome = Integer.parseInt(incomeAmount.getText());
         String targets = savingTarget.getValue().toString();
         String noPercentT = targets.replace("%", "");
@@ -230,7 +232,8 @@ catch(NumberFormatException nfe){
         int monthlyInc = biweekIncome * 2;
         double actualTSaved = .01 * targetSaved;
         double goal = monthlyInc * actualTSaved;
-      /* double currentAmount = monthly inc - expenses array total;
+        System.out.println(goal);
+        /* double currentAmount = monthly inc - expenses array total;
         if(goal < currentAmount){
         underperforming.setFill(Color.RED);  
         }else if(goal == currentAmount){
@@ -241,42 +244,51 @@ catch(NumberFormatException nfe){
         
         
         waiting on update array to accuratley depict expenses
-        */
+         */
     }
-    
-    public void save(ActionEvent event){
-        String incomeTest=incomeAmount.getText();
-boolean incorrect = true;
-while(incorrect){
-try{Double.parseDouble(incomeTest);incorrect = false;}
-catch(NumberFormatException nfe){
-   incomeTest="0";
-   Alert a = new Alert(AlertType.ERROR);
-   a.setContentText("Invalid data type");
-   a.show();
- }
-}
 
-String amountTest=amount.getText();
-boolean incorrect2 = true;
-while(incorrect2){
-try{Double.parseDouble(amountTest);incorrect2 = false;}
-catch(NumberFormatException nfe){
-   amountTest="0";
-   Alert a = new Alert(AlertType.ERROR);
-   a.setContentText("Invalid data type");
-   a.show();
- }
-}
-    }
-    
-    public void load(ActionEvent event){
+    public void save(ActionEvent event) {
+        String incomeTest = incomeAmount.getText();
+        boolean incorrect = true;
+        while (incorrect) {
+            try {
+                Double.parseDouble(incomeTest);
+                incorrect = false;
+            } catch (NumberFormatException nfe) {
+                incomeTest = "0";
+                Alert a = new Alert(AlertType.ERROR);
+                a.setContentText("Invalid data type");
+                a.show();
+            }
+        }
+
+        String amountTest = amount.getText();
+        boolean incorrect2 = true;
+        while (incorrect2) {
+            try {
+                Double.parseDouble(amountTest);
+                incorrect2 = false;
+            } catch (NumberFormatException nfe) {
+                amountTest = "0";
+                Alert a = new Alert(AlertType.ERROR);
+                a.setContentText("Invalid data type");
+                a.show();
+            }
+        }
+        
+        
+        
         
     }
-    
-    public void delete(ActionEvent event){
-        
+
+    public void load(ActionEvent event) {
+
     }
+
+    public void delete(ActionEvent event) {
+
+    }
+
     /*
     update button method
     
@@ -299,46 +311,34 @@ catch(NumberFormatException nfe){
     as user exceeds target, the other fills are null and overperforming is set to green fill.
     
     
-    */
-    
-    
-    /*
+     */
+ /*
     save button method
     
     This should pull the array items from wherever update stores and puts them in an integrated text file. an example of how to do this is the dungeons and damned application I put in discord
     
-    */
-    
-    
-    /*
+     */
+ /*
     load button method
     
     this should pull all the lines from the integrated text file and replace them in the array update uses and then restore all the data points and budget status
     an example of loading and saving to integrated text files is in the dungeons and damned app in discord
     
-    */
-    
-    /*
+     */
+ /*
     delete button method
     
     this should clear all data stored in the integrated text file and clear the update array and set all fields to default values
     
     
-    */
-    
+     */
     // the budget goals is just to include goals and doesn't link to anything, it is basically finished.
-    
-    
-    
-    
-    
-    
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
         launch(args);
     }
-    
+
 }
